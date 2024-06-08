92f8caac57623bddc0427a2be26c26db2cced445
NaleemNazzar <naleemnazzar96@gmail.com>
2024-06-08 23:18:39 +0530

    Part 1 Done

--- MainClass.cs
+++ MainClass.cs
@@ -5,6 +5,11 @@ using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+using System.Collections;
+using System.Reflection;
+using System.IO;
 
 namespace POS_System
 {
@@ -25,15 +30,79 @@ namespace POS_System
             SqlDataAdapter da = new SqlDataAdapter(cmd);
             da.Fill(dt);
 
-                if (dt.Rows.Count > 0)
-                {
-                    isValid = true;
-                    //user = dt.Rows[0]["uName"].ToString();
-                }
-                return isValid;
-            }
+            if (dt.Rows.Count > 0)
+            {
+                isValid = true;
+                USER = dt.Rows[0]["uName"].ToString();
 
-            
+                Byte[] imageArray = (byte[])dt.Rows[0]["uImage"];
+                byte[] imageByteArry = imageArray;
+                IMG = Image.FromStream(new MemoryStream(imageArray));
+            }
+            return isValid;
         }
+        public static void StopBuffering(Panel ctr, bool doubleBuffer)
+        {
+            try
+            {
+                typeof(Control).InvokeMember("DoubleBuffered",
+                    BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.SetProperty,
+                    null, ctr, new object[] { doubleBuffer });
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        // Create property for username
+
+        public static string user;
+
+        public static string USER
+        {
+            get { return user; }
+            private set { user = value; }
+        }
+
+        //for user image
+        public static Image img;
+
+        public static Image IMG
+        {
+            get { return img; }
+            private set { img = value; }
+        }
+
+
+        //Method for curd Operation
+
+        public static int SQL(string qry, Hashtable ht)
+        {
+            int res = 0;
+
+            try
+            {
+                SqlCommand cmd = new SqlCommand(qry, con);
+                cmd.CommandType = CommandType.Text;
+
+                foreach (DictionaryEntry item in ht)
+                {
+                    cmd.Parameters.AddWithValue(item.Key.ToString(), item.Value);
+                }
+                if (con.State == ConnectionState.Closed) { con.Open(); }
+                res = cmd.ExecuteNonQuery();
+                if (con.State == ConnectionState.Open) { con.Close(); }
+            }
+            catch (Exception ex)
+
+            {
+                MessageBox.Show(ex.ToString());
+                con.Close();
+            }
+            return res;
+        }
+        //For Loading data from database
     }
+}
 
